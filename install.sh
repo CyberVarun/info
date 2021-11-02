501b247d3be5b21b14467671e1d2dfaedfb406df
CyberVarun <varunjagtap424@gmail.com>
2021-11-02 17:30:31 +0530

    Added colours, shorthand for all options, -h help option and automated installer.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $? != 0 ]];
+then
+	echo "This must be run with root/sudo privileges"
+	echo "Try again"
+fi
+
+if [[ $1 == "--uninstall" ]];
+then
+	rm -rf /usr/local/bin/info
+	echo "Removed previous files"
+	exit
+fi
+
+cp info /usr/local/bin/
+echo "Installed successfully."
