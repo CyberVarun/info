841092027d6b1d334d8d58510166c86a321bedf7
CyberVarun <varunjagtap424@gmail.com>
2022-02-08 11:02:55 +0530

    Added colors and Modified output format.

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-if [[ $? != 0 ]];
-then
-	echo "This must be run with root/sudo privileges"
-	echo "Try again"
+grn='\033[32m'
+ylo='\033[1;33m'
+red='\033[31m'
+
+if [[ $UID != 0 ]];
+then 
+	echo -e "${ylo}[!] Run with sudo privileges."
 	exit
 fi
 
 if [[ $1 == "--uninstall" ]];
 then
 	rm -rf /usr/local/bin/info
-	echo "Removed previous files"
+	echo -e "${red}[!] Uninstalled"
 	exit
 fi
 
-cp info /usr/local/bin/
-echo "Installed successfully."
+cp info /usr/local/bin/ > /dev/null 2>&1
+
+if [[ $? == 0 ]];
+then
+	echo -e "${grn}[*] Installed successfully."
+	exit
+else
+	echo -e "${red}[!] Installation Failed. Check if there is already a named info in /usr/local/bin."
+	exit
+fi
